f6191bd981e9a4e0a32c05d73fb0505c43c2e0c0
Markus Ressel <mail@markusressel.de>
2019-01-28 14:08:50 +0100

    added python highlighter

---
+++ python/src/main/java/de/markusressel/kodehighlighter/language/python/colorscheme/DarkBackgroundColorScheme.kt
@@ -0,0 +1,37 @@
+package de.markusressel.kodehighlighter.language.python.colorscheme
+
+import android.graphics.Color
+import android.text.style.ForegroundColorSpan
+import de.markusressel.kodehighlighter.core.StyleFactory
+import de.markusressel.kodehighlighter.core.SyntaxColorScheme
+import de.markusressel.kodehighlighter.core.SyntaxHighlighterRule
+import de.markusressel.kodehighlighter.language.python.rule.*
+import java.util.Collections.emptySet
+
+/**
+ * A dark color scheme for java
+ */
+class DarkBackgroundColorScheme : SyntaxColorScheme {
+
+    override fun getStyles(type: SyntaxHighlighterRule): Set<StyleFactory> {
+        return when (type) {
+            is DefKeyword,
+            is ClassKeywordRule,
+            is TypeKeywordRule,
+            is InternalFunctionsRule,
+            is SelfKeywordRule,
+            is OtherKeywordsRule,
+            is ExceptRule -> {
+                setOf({ ForegroundColorSpan(Color.parseColor("#FF6D00")) })
+            }
+            is DecoratorRule -> {
+                setOf({ ForegroundColorSpan(Color.parseColor("#FBC02D")) })
+            }
+            is CommentRule -> {
+                setOf({ ForegroundColorSpan(Color.parseColor("#33691E")) })
+            }
+            else -> emptySet()
+        }
+    }
+
+}
---
+++ python/src/main/java/de/markusressel/kodehighlighter/language/python/rule/ReturnKeywordRule.kt
@@ -0,0 +1,16 @@
+package de.markusressel.kodehighlighter.language.python.rule
+
+import android.text.Spannable
+import de.markusressel.kodehighlighter.core.SyntaxHighlighterRule
+
+class ReturnKeywordRule : SyntaxHighlighterRule {
+
+    override fun findMatches(spannable: Spannable): List<MatchResult> {
+        return PATTERN.findAll(spannable).toList()
+    }
+
+    companion object {
+        val PATTERN = "return(?=\\s)".toRegex()
+    }
+
+}
