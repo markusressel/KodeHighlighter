5f050d9046943cb6f57bae436e6d00dd7c980207
Markus Ressel <mail@markusressel.de>
2019-07-02 01:00:43 +0200

    added single line comments

--- python/src/main/java/de/markusressel/kodehighlighter/language/python/rule/CommentRule.kt
+++ python/src/main/java/de/markusressel/kodehighlighter/language/python/rule/CommentRule.kt
@@ -7,11 +7,13 @@ import de.markusressel.kodehighlighter.core.rule.SyntaxHighlighterRule
 class CommentRule : SyntaxHighlighterRule {
 
     override fun findMatches(text: CharSequence): List<RuleMatch> {
-        return RuleHelper.findRegexMatches(text, PATTERN)
+        return RuleHelper.findRegexMatches(text, SINGLE_LINE_PATTERN) +
+                RuleHelper.findRegexMatches(text, MULTI_LINE_PATTERN)
     }
 
     companion object {
-        val PATTERN = "('{3}|\"{3})[\\s\\S]*?\\1".toRegex(RegexOption.MULTILINE)
+        val MULTI_LINE_PATTERN = "('{3}|\"{3})[\\s\\S]*?\\1".toRegex(RegexOption.MULTILINE)
+        val SINGLE_LINE_PATTERN = "\\s*#.*".toRegex(RegexOption.MULTILINE)
     }
 
 }
