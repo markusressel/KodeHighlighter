4de4f7c6e366d01cee1b0a149f4a752c7f6dcc98
Markus Ressel <mail@markusressel.de>
2024-03-16 16:39:14 +0100

    add parameters to pass through

--- core/src/main/java/de/markusressel/kodehighlighter/core/ui/KodeText.kt
+++ core/src/main/java/de/markusressel/kodehighlighter/core/ui/KodeText.kt
@@ -7,6 +7,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
 import de.markusressel.kodehighlighter.core.LanguageRuleBook
 import de.markusressel.kodehighlighter.core.colorscheme.ColorScheme
 import de.markusressel.kodehighlighter.core.util.AnnotatedStringHighlighter
@@ -28,6 +34,17 @@ fun KodeText(
     languageRuleBook: LanguageRuleBook,
     colorScheme: ColorScheme<SpanStyle>,
     textColor: Color = LocalTextStyle.current.color,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
 ) {
     val composeHighlighter by remember(languageRuleBook, colorScheme) {
         mutableStateOf(
@@ -50,5 +67,16 @@ fun KodeText(
         modifier = modifier,
         text = annotatedText,
         color = textColor,
+        fontSize = fontSize,
+        fontFamily = fontFamily,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        minLines = minLines,
+        onTextLayout = onTextLayout,
     )
 }
